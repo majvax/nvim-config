0347b0659050fef92ab053e8df917958fbf7b69f
majvax <majvax.contact@gmail.com>
2024-01-14 21:51:14 +0100

    nvchad config

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,18 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "bash",
+    "markdown",
+    "markdown_inline",
+    "regex"
+  },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
